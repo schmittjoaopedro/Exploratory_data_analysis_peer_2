40d88af5754e7f11184fe0f913f433c4f8f7ee6e
Joao Pedro Schmitt <jschmitt@weg.net>
2016-08-22 16:40:01 -0300

    adicionado plot3

---
+++ plot3.R
@@ -0,0 +1,37 @@
+# Libraries
+library(dplyr)
+library(ggplot2)
+
+# Common files and URI's
+urlFile <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+zipFile <- "data.zip"
+sourceFile <- "Source_Classification_Code.rds"
+summaryFile <- "summarySCC_PM25.rds"
+
+# Obtain the data in the files
+if(!file.exists(zipFile)) {
+  download.file(urlFile, destfile = zipFile)
+}
+if(!file.exists(sourceFile) | !file.exists(summaryFile)) {
+  unzip(zipFile) 
+}
+
+# Read the files
+NEI <- readRDS(summaryFile)
+SCC <- readRDS(sourceFile)
+
+# Prepare the data
+filtered <- NEI %>% 
+  filter(fips == "24510") %>%
+  mutate(type = as.factor(type), year = as.factor(year)) %>%
+  select(year, type, Emissions)
+
+# Plot the graphs
+g <- ggplot(filtered, aes(x = year, y = log10(Emissions), fill = type))
+g + facet_grid(. ~ type) + 
+  geom_boxplot() + 
+  labs(x = "Year", y = expression(paste("Log"[10], " of Emissions PM"[2.5]))) +
+  ggtitle('Emissions by Type in Baltimore City, Maryland')
+
+# Flush the PNG file
+ggsave(file="plot3.png")
